1e03d2d51ac1962dbee2dd5356a96acd5225d358
pbao9 <pbao.bussiness@gmail.com>
2024-06-26 21:21:11 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
-<script setup></script>
+<script setup>
+import HeaderItem from './components/HeaderItem.vue'
+</script>
 
 <template>
+  <HeaderItem></HeaderItem>
+  <main></main>
   <div class="flex justify-center items-center h-[100vh]">
-    <h1 class="font-bold uppercase text-[50px]">Web Portfolio Đang được design</h1>
+    <h1 class="font-bold uppercase text-[50px]">Web Portfolio - VUE - TailwindCSS</h1>
   </div>
 </template>
 
--- src/components/HeaderItem.vue
+++ src/components/HeaderItem.vue
@@ -1,3 +1,17 @@
 <template>
-  <header></header>
+  <header class="w-100 bg-white border-b-[1px] h-[90px] flex justify-center">
+    <div class="flex flex-row justify-between items-center h-[100%] container">
+      <div class="basis-1/6 font-mon text-[2rem] font-bold">
+        PBao<span class="text-green-800">9</span>
+      </div>
+      <div class="basis-1/4 bg-white">
+        <ul class="flex gap-[20px] font-mon">
+          <li><a href="#introduce" class="nav-link">Introduce</a></li>
+          <li><a href="#service" class="nav-link">Service</a></li>
+          <li><a href="#product" class="nav-link">Product</a></li>
+          <li><a href="#contact" class="nav-link">Contact</a></li>
+        </ul>
+      </div>
+    </div>
+  </header>
 </template>
